63b73feaba13dc9967c0cc7fdb036eb1e451dbe5
nicklindenmeyer <149104974+nicklindenmeyer@users.noreply.github.com>
2023-10-27 14:45:16 -0300

    Add files via upload

--- dash.py
+++ dash.py
@@ -23,48 +23,48 @@ data = get_data_from_thingspeak('2302302', 'A5W7L8EBQ74NJ3YH')
 
 df = pd.DataFrame(data['feeds'])
 
-divisao0 = df["created_at"].str.split("T")
+#divisao0 = df["created_at"].str.split("T")
 
-data = divisao0.str.get(0)
-df["Data"] = data
-df["Data"] = pd.to_datetime(df["Data"])
+#data = divisao0.str.get(0)
+#df["Data"] = data
+#df["Data"] = pd.to_datetime(df["Data"])
 
-horario_utc = divisao0.str.get(1)
-df["Horario UTC"] = horario_utc
-divisao1 = df["Horario UTC"].str.split(":")
-hora = divisao1.str.get(0)
-minuto = divisao1.str.get(1)
+#horario_utc = divisao0.str.get(1)
+#df["Horario UTC"] = horario_utc
+#divisao1 = df["Horario UTC"].str.split(":")
+#hora = divisao1.str.get(0)
+#minuto = divisao1.str.get(1)
 
-horario = hora+":"+minuto
+#horario = hora+":"+minuto
 
-df["Horario"] = horario
+#df["Horario"] = horario
 
-df["Temperatura"] = df["field1"]
-df["Umidade"] = df["field2"]
-df["Pressao Atmosferica"] = df["field3"]
+#df["Temperatura"] = df["field1"]
+#df["Umidade"] = df["field2"]
+#df["Pressao Atmosferica"] = df["field3"]
 
-df = df.drop("entry_id", axis=1)
+#df = df.drop("entry_id", axis=1)
 
 st.title("Mês Atual")
 
 st.write(df)
 
-df["Day"] = df["Data"].apply(lambda x: str(x.year)+"/"+str(x.month)+"/"+str(x.day))
-day = st.sidebar.selectbox("Selecione o dia", df["Day"].unique())
+#df["Day"] = df["Data"].apply(lambda x: str(x.year)+"/"+str(x.month)+"/"+str(x.day))
+#day = st.sidebar.selectbox("Selecione o dia", df["Day"].unique())
 
-df_filtered = df[df["Day"] == day]
+#df_filtered = df[df["Day"] == day]
 
-col1, = st.columns(1)
-col2, = st.columns(1) 
-col3, = st.columns(1)
+#col1, = st.columns(1)
+#col2, = st.columns(1) 
+#col3, = st.columns(1)
 
-fig_temperatura_dia = px.line(df_filtered, x="Horario", y="Temperatura", title="Gráfico de Temperatura por Dia")
-col1.plotly_chart(fig_temperatura_dia, use_container_width=True)
+#fig_temperatura_dia = px.line(df_filtered, x="Horario", y="Temperatura", title="Gráfico de Temperatura por Dia")
+#col1.plotly_chart(fig_temperatura_dia, use_container_width=True)
 
-fig_umidade_dia = px.line(df_filtered, x="Horario", y="Umidade", title="Gráfico de Umidade do Ar por Dia")
-col2.plotly_chart(fig_umidade_dia, use_container_width=True)
+#fig_umidade_dia = px.line(df_filtered, x="Horario", y="Umidade", title="Gráfico de Umidade do Ar por Dia")
+#col2.plotly_chart(fig_umidade_dia, use_container_width=True)
 
-fig_pressao_dia = px.line(df_filtered, x="Horario", y="Pressao Atmosferica", title="Gráfico de Pressão Atmosférioca por Dia")
-col3.plotly_chart(fig_pressao_dia, use_container_width=True)
+#fig_pressao_dia = px.line(df_filtered, x="Horario", y="Pressao Atmosferica", title="Gráfico de Pressão Atmosférioca por Dia")
+#col3.plotly_chart(fig_pressao_dia, use_container_width=True)
 
-print(df)
+#print(df)
